a25dc814d6d34c46ba1259ed2f57a319e722c615
Jorge Quesada <88360796+CheCR@users.noreply.github.com>
2022-10-03 16:42:50 -0600

    Add files via upload

---
+++ Test4/index.js
@@ -0,0 +1,21 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSoruce = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSoruce);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSoruce2 = "images/" + randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSoruce2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!"
+}
